41ac0bdc950bef085076c73992f3bcd6ac220c0b
Loki <lokesh.mydilse@gmail.com>
2019-05-15 18:43:37 -0500

    Update MyMaven.groovy

--- src/com/sq/MyMaven.groovy
+++ src/com/sq/MyMaven.groovy
@@ -3,7 +3,8 @@ package com.sq
 
 def maven(mavenvar){
   try{
-    sh "echo ${mavenvar}"
+    sh "echo hi"
+    sh "${mavenvar}"
   }catch(err) {
     echo "Resulted in error"
     println err.toString()
